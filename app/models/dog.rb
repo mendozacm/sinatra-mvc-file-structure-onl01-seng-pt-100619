e826b8a1aa18481e22b9ba219f29d2bfe813306b
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-01-27 00:30:26 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,6 +3,13 @@ class Dog
 attr_accessor :name, :breed, :age
 @@all = []
 
+def initialize(name, age, breed)
+    @name = name
+    @age = age
+    @breed = breed
+    @@all << self
+  end
+
 def self.all 
  @@all 
 end
